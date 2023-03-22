52379de4ca7028ff93615fe267ab4a4bfe71ce0b
effgrandad <effgrandad@gmail.com>
2023-03-22 14:37:03 -0700

    101 natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always success
+ */
+
+int main(void)
+{
+	int 1, z = 0;
+
+	while (1 < 1024)
+	{
+	if ((1 % 3 == 0) || (1 % 5 == 0))
+	{
+	z += i;
+	}
+	i++
+	}
+	printf("%d\n", z);
+	return (0);
+}

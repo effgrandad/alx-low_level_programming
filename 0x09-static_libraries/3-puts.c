436417404454de504133ad28e3100eded0340929
effgrandad <effgrandad@gmail.com>
2023-04-10 10:53:53 -0700

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description:
+ * Return: always 0
+ */
+int main(void)
+void _puts(char *str)
+{
+	puts("\"programming is like building a multilinual puzzle");
+	return (0);
+}
+

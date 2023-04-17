d669f9fdbb302c179cf5dbccf99e8605cd08fc92
effgrandad <effgrandad@gmail.com>
2023-04-17 16:39:18 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs
+ * @d: pointer to free a dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

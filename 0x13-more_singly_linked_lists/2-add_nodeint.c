874777267b8000f53e313670961172f2246bd027
effgrandad <effgrandad@gmail.com>
2023-10-24 15:44:10 -0700

    add_node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,20 +7,11 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new;
 
-		if (head == NULL)
-			return (0);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+	return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	if (*head == NULL)
-		new_node->next = NULL;
-	else
-		new_node->next = *head;
-	new_node->n = n;
-	*head = new_node;
-	return (0);
-
+	new->n = n;
 }

93d05e0e6b7e5f7e157639c3cbd1145631a72bf1
effgrandad <effgrandad@gmail.com>
2023-10-26 11:31:58 -0700

    bit manupilation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,33 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int h = 0;
-	unsigned int sum; length; h; powa, j;
-
-	powa = 1;
-	base = 2;
-	sum = 0;
+	size_t h = 0;
+	unsigned int length;
+	size_t j = 0;
+	size_t powa = 1;
+	size_t sum = 0;
+	int base = 2;
 
 	if (b == NULL)
 		return (0);
 
-	for (length = 0; b[length] !='\0')
-			sum++;
+	for (length = 0; b[length] != '\0'; length++)
+
+	if (length == 1 && (b[0] == '0' || b[0] == '1'))
+		return (b[0] - 48);
+
+	for (; b[h] != '\0'; h++)
+	{
+		if (b[h] != '0' && b[h] != '1')
+		{
+			return (0);
+		}
+
+		for (j = length - 1; j > 0; j--)
+		powa = powa * base;
+		sum = sum + (powa * (b[h] - 48));
+		length--;
+		powa = 1;
 	}
 	return (sum);
 }
